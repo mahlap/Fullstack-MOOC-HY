00a2eb91cf74b0ea6e8523d1f3e6f7ce95f2958a
pontusmahla@live.fi <pontusmahla@live.fi>
2021-07-06 19:18:51 +0300

    2.4 tehty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+
+
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(courses => 
+        <Part key={courses.id} course={courses} />
+        )}
+    </div>
   )
 }
 
@@ -23,56 +35,80 @@ const Part = ({ course }) => {
   )
 }
 
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(courses => 
-        <Part key={courses.id} course={courses} />
-        )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
+const Courses = ({ courses }) => {
   return (
     <>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course} />
+      {courses.map(course =>
+    <CourseParts key={course.id} course={course}/>
+    )}
     </>
   )
 }
 
+const CourseParts = ({ course }) => {
+  return (
+    <>
+    <Header course={course}/>
+    <Content course={course}/>
+    <Total course={course}/>
+    </>
+  )
+} 
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Coolest web development courses ever</h1>
+      <Courses courses={courses} />
     </div>
   )
 }
 
 export default App
 
-//2.3 tehty
+//2.4 tehty
