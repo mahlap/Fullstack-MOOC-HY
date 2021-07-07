6197c1aec9c7bb592148f33294ff4bdf4b1c70d2
pontusmahla@live.fi <pontusmahla@live.fi>
2021-07-07 17:55:27 +0300

    puhelinluettelon eka osa tehty

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const Numbers = ({ persons }) => {
+
+  return (
+    <>
+      {persons.map(person =>
+        <p key={person.name}>{person.name}</p>
+        )}
+    </>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameAdding = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameAdding}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
+//2.6 tehty
