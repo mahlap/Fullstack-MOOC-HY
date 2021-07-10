7a42d8486caa815e88b80307fb982dab7d4fad29
pontusmahla@live.fi <pontusmahla@live.fi>
2021-07-10 11:01:41 +0300

    2.9 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,23 +1,17 @@
 import React, { useState } from 'react'
 
-const Numbers = ({ persons }) => {
-
-  return (
-    <>
-      {persons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
-        )}
-    </>
-  )
-}
-
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number:  '040-123123'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ showAll, setShowAll ] = useState(true) 
+  const [ compare, setCompare ] = useState('') 
+  
 
   const handleNameAdding = (event) => {
     setNewName(event.target.value)
@@ -42,11 +36,27 @@ const App = () => {
   const handleNumberAdding = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleFilter = (event) => {
+    setCompare(event.target.value)
+    if(event.target.value === '') {
+      setShowAll(true)
+    } else {
+      setShowAll(false)
+    }
+  }
+
+  const numbersToShow = showAll
+  ? persons
+  : persons.filter(person => person.name.toUpperCase().includes(compare.toUpperCase())) //does a better method exist?
+
   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onChange={handleFilter}></input></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameAdding}/>
@@ -57,11 +67,15 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <div>
+      {numbersToShow.map(person =>
+         <p key={person.name}>{person.name} {person.number}</p>
+         )}
+      </div>
     </div>
   )
 
 }
 
 export default App
-//2.8 tehty
+//2.9 tehty 
